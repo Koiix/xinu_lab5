08fe599cf1dbb2155bc9a4dc20a86ee3a093b515
Joel Kreider <kreiderj@purdue.edu>
2019-04-21 14:54:49 -0400

    disconnect

--- system/pipdisconnect.c
+++ system/pipdisconnect.c
@@ -1,6 +1,14 @@
 #include <xinu.h>
 
 status pipdisconnect(did32 devpipe) {
-    // LAB2: TODO
+	int32 mask = disable();
+	if(isbadpipe(devpipe)){
+		return SYSERR;
+	}
+  if(pipe_table[devpipe] != PIPE_CONNECTED){
+		return SYSERR;
+	}
+	pipe_table[devpipe] = PIPE_USED;
+	enable(mask);
 	return OK;
 }
