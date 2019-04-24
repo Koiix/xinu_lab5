ae4af7bbb9cf3e2684f8d4cc0b04742ee6f63cdf
Joel Kreider <kreiderj@purdue.edu>
2019-04-24 01:19:02 -0400

    more bug fixes xd

--- system/pipcreate.c
+++ system/pipcreate.c
@@ -3,10 +3,10 @@
 did32 pipcreate() {
     int32 mask = disable();
     /* Traverse the list of pipe devices to find one free for use */
-    for(int32 minor = 0; minor < MAX_PIPES; minor++){
-      if(pipe_tables[minor].pipe_state_t == PIPE_FREE){
+    for(int32 minor = 0; minor < MAXPIPES; minor++){
+      if(pipe_table[minor].state == PIPE_FREE){
         /* Pipe is free for use, so we take the pipe */
-        struct pipe_t * pipeptr = &pipe_tables[minor];
+        struct pipe_t * pipeptr = &pipe_table[minor];
         /* find pipe device entry with same dvminor as index in pipe table */
         pipeptr->pipid = PIPELINE0 + minor;
         pipeptr->pipe_state_t = PIPE_USED;
