7a1c02e268b473e476049af5fc8630910e6efa00
Joel Kreider <kreiderj@purdue.edu>
2019-04-24 01:19:46 -0400

    fuck it i cant compile on this machine

--- system/pipcreate.c
+++ system/pipcreate.c
@@ -9,7 +9,7 @@ did32 pipcreate() {
         struct pipe_t * pipeptr = &pipe_table[minor];
         /* find pipe device entry with same dvminor as index in pipe table */
         pipeptr->pipid = PIPELINE0 + minor;
-        pipeptr->pipe_state_t = PIPE_USED;
+        pipeptr->state = PIPE_USED;
         pipeptr->owner = currpid;
         return pipeptr->pipid;
       }
