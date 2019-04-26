3052b93004989344da59edb1988d63ecd12bd90c
Joel Kreider <kreiderj@purdue.edu>
2019-04-26 00:44:55 -0400

    maybe fix?

--- system/pipconnect.c
+++ system/pipconnect.c
@@ -7,8 +7,8 @@ status pipconnect(did32 devpipe , pid32 writer, pid32 reader) {
     restore(mask);
     return SYSERR;
 	}
-  struct pipe_t pipe = pipe_table[devtab[devpipe].dvminor];
-  if(pipe.state == PIPE_CONNECTED || pipe.state == PIPE_FREE || pipe.state == PIPE_SEMICONNECTED){
+  struct pipe_t * pipe = &pipe_table[devtab[devpipe].dvminor];
+  if(pipe->state == PIPE_CONNECTED || pipe->state == PIPE_FREE || pipe->state == PIPE_SEMICONNECTED){
     if(PIP_DEBUG) PIP_ERR("connect");
     restore(mask);
     return SYSERR;
@@ -16,11 +16,11 @@ status pipconnect(did32 devpipe , pid32 writer, pid32 reader) {
   if(PIP_DEBUG){
     kprintf("connecting writer: %s with pid: %d to reader: %s with pid: %d using pipe: %d (Device %d)\n", proctab[writer].prname, writer, proctab[reader].prname, reader, devtab[devpipe].dvminor, devpipe);
   }
-  pipe.state = PIPE_CONNECTED;
-  pipe.reader = reader;
-  pipe.writer = writer;
-  pipe.to_read = semcreate(0);  /* Consumer semaphore starts at 0 */
-  pipe.to_write = semcreate(PIPE_SIZE); /* Producer semaphore starts at buffer size */
+  pipe->state = PIPE_CONNECTED;
+  pipe->reader = reader;
+  pipe->writer = writer;
+  pipe->to_read = semcreate(0);  /* Consumer semaphore starts at 0 */
+  pipe->to_write = semcreate(PIPE_SIZE); /* Producer semaphore starts at buffer size */
 
 	restore(mask);
 	return OK;
